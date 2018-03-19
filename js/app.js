f716b2f4656bbd61312e99a27d9a530a3af18934
msicakkanli <msicakkanli@protein.tech>
2018-03-19 21:05:56 +0300

    final commit

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@ $(document).ready(function () {
 $('#title').on('click',function () {
     let jobTitle = $(this).val();
     if(jobTitle === "other") {
-      let jobRole = '<input type="text" id="job" name="job_title" placeholder="Your job role">';
+      let jobRole = '<input type="text" id="other-title" name="job_title" placeholder="Your Job Role">';
       $('#info').append(jobRole); 
     } else {
-      $('#job').remove();  
+      $('#other-title').remove();  
     }
 })
 
@@ -38,20 +38,20 @@ $('#design').on('click',function () {
 const selectConferance = document.querySelector('.activities');
 const conferanceList = Array.from(selectConferance.getElementsByTagName("input"))
 
+//change event for conferance checkbox
 selectConferance.addEventListener('change', (e) => {
-
     selectHideConferance(e);
     calulateTotal(e);
-    
 })
 
 
-
+// disable same time conferance control 
 function selectHideConferance(e) {
     const checkbox = e.target;
     const checked = checkbox.checked;
     if (checkbox.name === "js-frameworks" && checked) {
         conferanceList[3].disabled = true;
+        
     }
     ;
     if (checkbox.name === "js-libs" && checked) {
@@ -80,6 +80,7 @@ function selectHideConferance(e) {
     }
 }
 
+//calculate selected conferance price
 function calulateTotal(e) {
     const checkbox = e.target;
     const checked = checkbox.checked;
@@ -103,3 +104,101 @@ function calulateTotal(e) {
     
 }
 
+
+//hide & show selected paymnet option 
+
+$('#paypal').hide();
+$('#bitcoin').hide();
+
+let paymentMethod = document.querySelector('#payment');
+
+paymentMethod.addEventListener('change',(e) =>{
+    let paymentType = $('#payment').val()
+    if (paymentType === "paypal") {
+        $('#credit-card').hide();
+        $('#bitcoin').hide();
+        $('#paypal').show();
+    } else if (paymentType === "bitcoin") {
+        $('#credit-card').hide();
+        $('#bitcoin').show();
+        $('#paypal').hide();
+    } else {
+        $('#credit-card').show();
+        $('#bitcoin').hide();
+        $('#paypal').hide();
+    };
+})
+
+//submit register form and control all label 
+
+$(document).on('click', 'button', function (e) {
+    //control name & emai label empty state 
+    let name = $('#name').val();
+    let email = $('#mail').val();
+    let testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+    
+    if (name === "") {
+        
+        $('#name').prev().css("color", "red");
+        $('#name').prev().first().text('Name: (please provide your name)');
+        $('#name').css("border-color", "red");
+    }
+    
+    if (testEmail.test(email)) {
+        
+    } 
+    else {
+        $('#mail').prev().css("color", "red");
+        $('#mail').prev().first().text('Email: (please provide a valid  e-mail address)');
+        $('#mail').css("border-color", "red");
+    }
+    //control conferance section selected option
+    let clickCount = 0; 
+    for (let i = 0 ; i< conferanceList.length; i ++)
+    {
+        if (conferanceList[i].checked === true) {
+            clickCount = clickCount + 1
+        }
+    };
+    if (clickCount === 0) {
+        
+        $('.activities').children().first().append('<p id="select"> Please select an Activity </p>');
+        $('#select').css("color","red")
+    }    
+    let design = $('#design').val();
+    let colorName = $('#color').val();
+   
+   // control selected t-shirt option  
+   if (design === "Select Theme" || colorName === "default") {
+       $('.shirt').children().first().append("<p id='forget'>Don't forget to pick up a T-Shirt</p>");
+       $('#forget').css("color","red")
+   }
+
+   //control credit card (card no, cvv, zip code) validation
+   let cvvNumber = $('#cvv').val();
+   let numberOfDigitsCvv=cvvNumber.toString().length
+   if ($.isNumeric(cvvNumber) &&  numberOfDigitsCvv === 3) {
+    // everything is fine :)
+   } else {
+       $('#cvv').prev().css("color", "red");
+   };
+
+   let zipCode = $('#zip').val();
+   let numberOfDigitsZip=zipCode.toString().length
+   if ($.isNumeric(zipCode) &&  numberOfDigitsZip === 5) {
+    // everything is fine :)
+   } else {
+       $('#zip').prev().css("color", "red");
+   };
+
+   let cardNumber = $('#cc-num').val();
+   let numberOfDigitsCardNumber=cardNumber.toString().length
+   if ($.isNumeric(cardNumber) && (12 < numberOfDigitsCardNumber && numberOfDigitsCardNumber < 17) ) {
+   
+   } else {
+       $('#cc-num').prev().css("color", "red");
+   };
+
+    e.preventDefault();
+    
+})
